9b32a5da8046a935e89fdf749e317678b8eefbe6
Leon <CassperZCPH>
2021-03-09 17:44:32 +0300

    install mariadb-connector

--- convert_project/main.py
+++ convert_project/main.py
@@ -5,18 +5,17 @@ import imp
 #module_file, pathname, description = imp.find_module("mariadb", [os.getcwd() + "/dependencies"])
 #module_mariadb = imp.load_module("mariadb", module_file, pathname, description)
 #sys.path.insert(0, os.getcwd() + "/dependencies")
-imp.load_source("mariadb", os.getcwd() + "/dependencies/mariadb/__init__.py")
-import mariadb
+mariadb_module = imp.load_source("mariadb", os.getcwd() + "/dependencies/mariadb/__init__.py")
 
 try:
-    db_connect = mariadb.connect(
+    db_connect = mariadb_module.connect(
         user="leon",
         password="dont_lock_my_pass",
         host="localhost",
         port=3306,
         database="products"
     )
-except mariadb.Error as err:
+except mariadb_module.Error as err:
     print(f"Error connecting to MariaDB Platform: {err}")
     sys.exit(1)
 
