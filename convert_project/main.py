61a86c0c7ce3facdde5c9774bcfe9c69a3784881
Leon <CassperZCPH>
2021-03-09 17:12:14 +0300

    load_source

--- convert_project/main.py
+++ convert_project/main.py
@@ -2,19 +2,21 @@ import configparser
 import os
 import sys
 import imp
-module_file, pathname, description = imp.find_module("mariadb", [os.getcwd() + "\dependencies"])
-module_mariadb = imp.load_module("_mariadb.cp36-win_amd64", module_file, pathname, description)
-#sys.path.insert(0, os.getcwd() + "\dependencies")
+#module_file, pathname, description = imp.find_module("mariadb", [os.getcwd() + "/dependencies"])
+#module_mariadb = imp.load_module("mariadb", module_file, pathname, description)
+#sys.path.insert(0, os.getcwd() + "/dependencies")
+imp.load_source("mariadb", os.getcwd() + "/dependencies/mariadb/__init__.py")
+import mariadb
 
 try:
-    db_connect = module_mariadb.connect(
+    db_connect = mariadb.connect(
         user="leon",
         password="dont_lock_my_pass",
         host="localhost",
         port=3306,
         database="products"
     )
-except module_mariadb.Error as err:
+except mariadb.Error as err:
     print(f"Error connecting to MariaDB Platform: {err}")
     sys.exit(1)
 
