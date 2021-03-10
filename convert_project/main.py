feb00bafb39fac7b9e58870e1821d9f768e3d0fa
Leon <CassperZCPH>
2021-03-10 11:03:40 +0300

    delete mariadb_module

--- convert_project/main.py
+++ convert_project/main.py
@@ -5,17 +5,18 @@ import imp
 #module_file, pathname, description = imp.find_module("mariadb", [os.getcwd() + "/dependencies"])
 #module_mariadb = imp.load_module("mariadb", module_file, pathname, description)
 #sys.path.insert(0, os.getcwd() + "/dependencies")
-mariadb_module = imp.load_source("mariadb", os.getcwd() + "/dependencies/mariadb/__init__.py")
+#mariadb_module = imp.load_source("mariadb", os.getcwd() + "\dependencies\mariadb-connector-c-3.1.9-centos7-amd64\lib\mariadb\libmariadb")
+import mariadb
 
 try:
-    db_connect = mariadb_module.connect(
+    db_connect = mariadb.connect(
         user="leon",
         password="dont_lock_my_pass",
         host="localhost",
         port=3306,
         database="products"
     )
-except mariadb_module.Error as err:
+except mariadb.Error as err:
     print(f"Error connecting to MariaDB Platform: {err}")
     sys.exit(1)
 
