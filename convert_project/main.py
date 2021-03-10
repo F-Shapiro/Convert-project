b3b437624487088a64c14365cd74bf4cb46edc00
Leon <CassperZCPH>
2021-03-10 16:03:18 +0300

    it is working!

--- convert_project/main.py
+++ convert_project/main.py
@@ -1,17 +1,11 @@
 import configparser
-import os
-import sys
-import imp
-#module_file, pathname, description = imp.find_module("mariadb", [os.getcwd() + "/dependencies"])
-#module_mariadb = imp.load_module("mariadb", module_file, pathname, description)
-#sys.path.insert(0, os.getcwd() + "/dependencies")
-#mariadb_module = imp.load_source("mariadb", os.getcwd() + "\dependencies\mariadb-connector-c-3.1.9-centos7-amd64\lib\mariadb\libmariadb")
 import mariadb
-
+import sys
+ 
 try:
     db_connect = mariadb.connect(
-        user="leon",
-        password="dont_lock_my_pass",
+        user="root",
+        password="",
         host="localhost",
         port=3306,
         database="products"
