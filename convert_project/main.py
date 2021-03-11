539942932a515705aa40f206484dd261d6f533da
Leon <CassperZCPH>
2021-03-11 14:56:31 +0300

    change sql request

--- convert_project/main.py
+++ convert_project/main.py
@@ -1,11 +1,14 @@
 # main.py
+import time
 import configparser
 from src_modules.DBManager import DBManager
 
 def main():
+    print(time.ctime())
     dbmanager = DBManager()
     dbmanager.getRecord()
     del dbmanager
+    print(time.ctime())
 
 if __name__ == "__main__":
     main()
--- convert_project/src_modules/DBManager.py
+++ convert_project/src_modules/DBManager.py
@@ -36,7 +36,9 @@ class DBManager:
 
     def getRecord(self):
         self.__cursor.execute(
-            f"SELECT id, title_img FROM products LIMIT {self.__step_limit}"
+            f'''SELECT id, title_img FROM products
+            WHERE title_img IS NOT NULL AND title_img REGEXP '^http:'
+            LIMIT {self.__step_limit}'''
         )
         self.__current_number_of_record += self.__step_limit
         rows = self.__cursor.fetchall()
